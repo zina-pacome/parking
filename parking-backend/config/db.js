546ce3cb00eb9fbe738f415e7546e26e0839d287
zina-pacome <pacomezone@gmail.com>
2026-04-28 13:01:37 +0300

    fix railway mysql connection

--- parking-backend/config/db.js
+++ parking-backend/config/db.js
@@ -1,13 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
