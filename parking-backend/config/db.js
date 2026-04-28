9fd1ae1208776b7837fb2839622617d7e286fbb2
zina-pacome <pacomezone@gmail.com>
2026-04-28 13:11:55 +0300

    fix railway mysql url parsing

--- parking-backend/config/db.js
+++ parking-backend/config/db.js
@@ -1,6 +1,16 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const dbUrl = new URL(process.env.DATABASE_URL);
+
+const pool = mysql.createPool({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace('/', ''),
+  port: dbUrl.port,
+  waitForConnections: true,
+  connectionLimit: 10
+});
 
 module.exports = pool;
